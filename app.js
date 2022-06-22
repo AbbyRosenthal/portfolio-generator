402c7a31485e7dedfd393e4349148f4455f055b5
Abby Rosenthal <adlundeen@gmail.com>
2022-06-22 14:29:02 -0500

    inquirer question

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-//NEED THIS TO RUN MODULES
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-//holds user command line arguments
-const profileDataArgs = process.argv.slice(2);
-//extract arguments and store them into distinct variables
-const [name, github] = profileDataArgs;
+inquirer
+.prompt([
+   {
+       type: 'input',
+       name: 'name',
+       message: 'What is your name?'
+   } 
+])
+.then(answers => console.log(answers));
+
+// //NEED THIS TO RUN MODULES
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
 
 
-//WRITE THE HTML FILE!!!
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    //stops execution of code if an error appears
-    if (err) throw new Error(err);
+// //WRITE THE HTML FILE!!!
+// fs.writeFile('./index.html', pageHTML, err => {
+//     //stops execution of code if an error appears
+//     if (err) throw (err);
 
-    console.log('Portfolio complete!  Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete!  Check out index.html to see the output!');
+// });
 
 
 
@@ -33,6 +42,11 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
 
 //good tool from what we learned in lesson one but we don't neeed for the project
+//holds user command line arguments
+// const profileDataArgs = process.argv.slice(2);
+//extract arguments and store them into distinct variables
+// const [name, github] = profileDataArgs;
+
 
 // const printProfileData = profileDataArr => {
 //   // This...
